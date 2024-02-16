ff725f9a48c31ebca7776213d7994b0628f4a456
derCamembert <vonkrc20@htl-kaindorf.at>
2024-02-16 08:38:46 +0100

    xml access added

--- src/main/java/at/kaindorf/customerdata/Main.java
+++ src/main/java/at/kaindorf/customerdata/Main.java
@@ -1,7 +1,12 @@
 package at.kaindorf.customerdata;
 
+import at.kaindorf.customerdata.pojos.DataHolder;
+import at.kaindorf.customerdata.xml.XML_Access;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        DataHolder dataHolder = XML_Access.getTheInstance().loadCustomerFromXmlFile();
     }
 }
--- src/main/java/at/kaindorf/customerdata/xml/XML_Access.java
+++ src/main/java/at/kaindorf/customerdata/xml/XML_Access.java
@@ -1,5 +1,36 @@
 package at.kaindorf.customerdata.xml;
 
-public class XML_Access {
+import at.kaindorf.customerdata.pojos.DataHolder;
+import jakarta.xml.bind.JAXB;
+
+import java.io.InputStream;
+
+public final class XML_Access {
+
+    private static XML_Access theInstance;
+
+    private XML_Access(){
+
+    }
+
+    public static XML_Access getTheInstance(){
+        if (theInstance ==null){
+            theInstance = new XML_Access();
+        }
+        return theInstance;
+    }
+
+    public DataHolder loadCustomerFromXmlFile(){
+        InputStream is = getClass().getResourceAsStream("/customer_data.xml");
+        if(is != null){
+            DataHolder dataHolder = JAXB.unmarshal(is, DataHolder.class);
+            System.out.println("Success!!! ##########");
+            return dataHolder;
+        } else {
+            System.out.println("Reading xml-file failed");
+            throw new RuntimeException("Reading xml-file failed");
+        }
+
+    }
 
 }
