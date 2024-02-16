d340d99fd92483736f2020a4380a021e0df63495
derCamembert <vonkrc20@htl-kaindorf.at>
2024-02-16 08:49:14 +0100

    xml access added

--- src/main/java/at/kaindorf/customerdata/pojos/City.java
+++ src/main/java/at/kaindorf/customerdata/pojos/City.java
@@ -1,10 +1,24 @@
 package at.kaindorf.customerdata.pojos;
 
 
-public class City {
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlElementWrapper;
+import lombok.Data;
 
-    private Long countryId;
-    private String countryName;
-    private String countryCode;
+import java.util.List;
+
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
+public class City {
+    private Long cityId;
+    private String cityName;
+    private String postalCode;
+
+    @XmlElementWrapper(name = "Customers")
+    @XmlElement(name = "Customer")
+    private List<Customer> customerList;
+
 
 }
--- src/main/java/at/kaindorf/customerdata/pojos/Country.java
+++ src/main/java/at/kaindorf/customerdata/pojos/Country.java
@@ -1,10 +1,23 @@
 package at.kaindorf.customerdata.pojos;
 
 
-public class Country {
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlElementWrapper;
+import lombok.Data;
 
-    private Long cityId;
-    private String cityName;
-    private String postalCode;
+import java.util.List;
+
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Country {
+    private Long countryId;
+    private String countryName;
+    private String countryCode;
+
+    @XmlElementWrapper(name = "Cities")
+        @XmlElement(name = "City")
+    private List<City> cities;
 
 }
